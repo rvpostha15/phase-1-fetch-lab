67af560f0c7df239a70dca508fc6831938991bc3
rvpostha15 <115106981+rvpostha15@users.noreply.github.com>
2022-12-19 13:19:31 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch(url)
+    .then(resp => resp.json())
+    .then(json => renderBooks(json))
 }
 
 function renderBooks(books) {
@@ -15,3 +17,14 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
+
+//function fetchBooks() should include a fetch request to GoT API ('https://anapioficeandfire.com/api/books')
+let url = 'https://anapioficeandfire.com/api/books'
+
+
+  //returned response should be converted to JSON
+  //then call 2nd fxn, renderBooks, 
+    //pass in the JSON-ified data as argument
+
+//open index.html page to see if you did it correctly, should see list of GoT titles on webpage
